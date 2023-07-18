efb25ac234feffa18091317489e16bc0e6c11d23
Beat94 <36741096+Beat94@users.noreply.github.com>
2023-07-18 10:54:04 +0000

    added function into cli start

--- cli-game/Cli.cs
+++ cli-game/Cli.cs
@@ -1,5 +1,13 @@
+using System;
+
 public class Cli
 {
+	public string? getStringCli(string message)
+	{
+		Console.Write(message + ": ");
+		return Console.ReadLine();
+	}
+
 	public void run()
 	{
 	
