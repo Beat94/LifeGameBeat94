2fb857b7045cc4c0ac36e2f7aa86b22316e196c2
Beat94 <36741096+Beat94@users.noreply.github.com>
2023-07-23 19:10:47 +0000

    ...

--- cli-game/Cli.cs
+++ cli-game/Cli.cs
@@ -8,14 +8,29 @@ public class Cli
 		return Console.ReadLine();
 	}
 
+	public string? menu()
+	{
+		bool rightChoice = false;
+		string? menuSelect = null;
+		while(rightChoice == false)
+		{
+			Console.WriteLine("Welcome to lifegame of Beat94");
+			Console.WriteLine("Main menu:");
+			Console.WriteLine("New game /t n");
+			Console.WriteLine("Load game /t l");
+			Console.WriteLine("Quit /t q");
+			menuSelect = getStringCli("Menu").ToLower();
+			if(menuSelect.Equals("n") || menuSelect.Equals("l") || menuSelect.Equals("q"))
+			{
+				rightChoice = true;
+			}
+		}
+
+		return menuSelect;
+	}
+
 	public void run()
 	{
-		string menuSelect;
-		Console.WriteLine("Welcome to lifegame of Beat94");
-		Console.WriteLine("Main menu:");
-		Console.WriteLine("New game /t n");
-		Console.WriteLine("Load game /t l");
-		Console.WriteLine("Quit /t q");
-		menuSelect = getStringCli("Menu");
+		string? menuSelect = menu();
 	}
 }
