dabef6247796a4b3df5d99c4cb8b5b9479590b5f
Beat94 <36741096+Beat94@users.noreply.github.com>
2023-07-30 12:01:48 +0000

    toolbox to cliTable

---
+++ cli-game/Toolbox.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+
+public class Toolbox
+{
+    public void cliTable(int cellsHorizontal, int cellsVertical, List<List<string>> list)
+    {
+        /* 
+         * \t ist für Tabulator
+         * \n ist für Enter
+         */
+        int count = list;
+
+    }
+}
