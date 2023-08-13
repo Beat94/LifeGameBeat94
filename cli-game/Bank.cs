c218619b5f2f347cfc34a072f1ce6556a9809d22
Beat Giuliano Rey <beat_g_94@hotmail.com>
2023-08-13 21:07:49 +0200

    extended Bank.cs + added BankAccount

--- cli-game/Bank.cs
+++ cli-game/Bank.cs
@@ -1,25 +1,33 @@
 public class Bank : IFinance
 {
-    public string name {get;}
+    public string nameBank {get;}
     public decimal positiveInterestBelow200k;
     public decimal positiveInterestOver200k;
     public decimal negativeInterestCredit;
     public decimal negativeInterestMortgage;
+    public List<BankAccount> BankAccountList = new List<BankAccount>();
 
     public Bank (
-        string name,
+        string nameBank,
         decimal positiveInterestBelow200k,
         decimal positiveInterestOver200k,
         decimal negativeInterestCredit,
         decimal negativeInterestMortgage)
     {
-        this.name = name;
+        this.nameBank = nameBank;
         this.positiveInterestBelow200k = positiveInterestBelow200k;
         this.positiveInterestOver200k = positiveInterestOver200k;
         this.negativeInterestCredit = negativeInterestCredit;
         this.negativeInterestMortgage = negativeInterestMortgage;
     }
 
+    public decimal getMaxCreditValue(decimal savingValue) => savingValue * 30;
+
+    public decimal getMaxMortgageValue(decimal income, decimal savings)
+    {
+        return 0;
+    }
+        
     public void newDay()
     {
         throw new NotImplementedException();
---
+++ cli-game/BankAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class BankAccount
+{
+    public long percentage { get; set; }
+    public Money amount { get; set; }
+}
