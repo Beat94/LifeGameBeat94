917674528004bd68d1e10c6f49b84471a59bc53a
Beat94 <36741096+Beat94@users.noreply.github.com>
2023-08-15 20:30:30 +0200

    Origin/beat94/bank2 (#22)
    * extended Bank.cs + added BankAccount
    
    * change in getMaxCreditValue

--- cli-game/Bank.cs
+++ cli-game/Bank.cs
@@ -1,24 +1,42 @@
 public class Bank : IFinance
 {
-    public string name {get;}
+    public string nameBank {get;}
     public decimal positiveInterestBelow200k;
     public decimal positiveInterestOver200k;
     public decimal negativeInterestCredit;
     public decimal negativeInterestMortgage;
+    public List<BankAccount> BankAccountList = new List<BankAccount>();
 
     public Bank (
-        string name,
+        string nameBank,
         decimal positiveInterestBelow200k,
         decimal positiveInterestOver200k,
         decimal negativeInterestCredit,
         decimal negativeInterestMortgage)
     {
-        this.name = name;
+        this.nameBank = nameBank;
         this.positiveInterestBelow200k = positiveInterestBelow200k;
         this.positiveInterestOver200k = positiveInterestOver200k;
         this.negativeInterestCredit = negativeInterestCredit;
         this.negativeInterestMortgage = negativeInterestMortgage;
     }
+
+    public decimal getMaxCreditValue(decimal savingValue) => savingValue * 30;
+
+    public (decimal, bool) getMaxMortgageValue(float income, float valueMoney)
+    {
+        bool outputbool = false;
+        float maxMortgage = valueMoney / 0.8f;
+        if (maxMortgage / (float)negativeInterestMortgage * 100 < income)
+        {
+            outputbool = true;
+        }
+
+        decimal maxMortgageDecimal = (decimal)(maxMortgage * 1000);
+        return (maxMortgageDecimal, outputbool);
+    }
+        
+
 
     public void newDay()
     {
