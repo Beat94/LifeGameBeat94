e7868864d2b6827b2c807480c0b252f4b0e2c9c7
Beat94 <36741096+Beat94@users.noreply.github.com>
2023-10-21 14:02:14 +0200

    finished Asset.cs

--- cli-game/Asset.cs
+++ cli-game/Asset.cs
@@ -1,10 +1,24 @@
 internal class Asset : IFinance
 {
-    internal Money value {get; set;}
-    internal decimal status {get; set;}
+    private Money valueFull {get; set;}
+    internal float status {get; set;}
+
+    internal Asset(Money valueFull, float status)
+    {
+        this.valueFull = valueFull;
+        this.status = status;
+    }
+
+    internal (Money valueFull, Money valueNow) getValue()
+    {
+        float value = valueFull.getValueFloat() / 100 * status;
+        value = value * 1000;
+        decimal valueDecimal = (decimal) value;
+        return (this.valueFull, new Money(valueDecimal));
+    }
 
     public void newDay()
     {
-
+        status = status * 0.99f;
     }
 }
