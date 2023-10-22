fb53ec34d684e2393352a423a5924ec4f1fe0517
Beat94 <36741096+Beat94@users.noreply.github.com>
2023-10-22 19:00:22 +0200

    created Test-Project (#46)

--- cli-game/Asset.cs
+++ cli-game/Asset.cs
@@ -1,15 +1,15 @@
-internal class Asset : IFinance
+public class Asset : IFinance
 {
-    private Money valueFull {get; set;}
-    internal float status {get; set;}
+    public Money valueFull {get; set;}
+    public float status {get; set;}
 
-    internal Asset(Money valueFull, float status)
+    public Asset(Money valueFull, float status)
     {
         this.valueFull = valueFull;
         this.status = status;
     }
 
-    internal (Money valueFull, Money valueNow) getValue()
+    public (Money valueFull, Money valueNow) getValue()
     {
         float value = valueFull.getValueFloat() / 100 * status;
         value = value * 1000;
---
+++ gameTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+namespace gameTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void HomeTestEqual()
+    {
+        Home home = new Home(new Money(3000000), 100, true, 3);
+
+        Assert.Equal(3000f, home.valueFull.getValueFloat());
+    }
+
+    [Fact]
+    public void HomeTestNotEqual()
+    {
+        Home home = new Home(new Money(3000000), 100, true, 3);
+
+        Assert.NotEqual(3f, home.valueFull.getValueFloat());
+    }
+}
