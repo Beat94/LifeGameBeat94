6b334cd58e4d2cf6371be2771fb1ba16cdee2c38
Beat94 <36741096+Beat94@users.noreply.github.com>
2023-10-22 20:32:42 +0200

    moved newDay-Function in AssetManager up

--- cli-game/AssetManager.cs
+++ cli-game/AssetManager.cs
@@ -2,17 +2,17 @@ internal class AssetManager:IFinance
 {
     private List<Asset> assetList = new List<Asset>();
 
-    public void addAsset(Asset asset)
-    {
-        assetList.Add(asset);
-    }
-
     public void newDay()
     {
         foreach(Asset asset in assetList)
         {
             asset.newDay();
         }
-    
     }
+    
+    public void addAsset(Asset asset)
+    {
+        assetList.Add(asset);
+    }
+
 }
