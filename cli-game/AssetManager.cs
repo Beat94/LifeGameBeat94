c852821d8218d32c4aa23be57b71291d32535520
Beat94 <36741096+Beat94@users.noreply.github.com>
2023-10-29 22:41:58 +0100

    added getAssetList function

--- cli-game/AssetManager.cs
+++ cli-game/AssetManager.cs
@@ -15,4 +15,16 @@ internal class AssetManager:IFinance
         assetList.Add(asset);
     }
 
+    public List<(string, string)> getAssetList()
+    {
+        List<(string,string)> AssetListOut = new();
+
+        for(int i = 0; i < assetList.Count; i++)
+        {
+            AssetListOut.Add((assetList[i].GetType().ToString(), i.ToString()));
+        }
+
+        return AssetListOut;
+    }
+
 }
