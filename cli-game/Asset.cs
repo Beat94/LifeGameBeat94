d2248a58ac2da7604b33fcba700a9b023d6111cd
Beat94 <36741096+Beat94@users.noreply.github.com>
2023-11-05 21:53:09 +0100

    added Features

--- cli-game/Asset.cs
+++ cli-game/Asset.cs
@@ -21,4 +21,33 @@ public class Asset : IFinance
     {
         status = status * 0.99f;
     }
+
+    public Money repairPrice()
+    {
+        (Money vFull, Money vNow) = getValue();
+        decimal valueMax = valueFull.getValueDecimal();
+        decimal valueNow = vNow.getValueDecimal();
+        decimal valueToPay = 0;
+
+        // to evaluate how high the price of repair is. 
+        if(status < 80)
+        {
+            valueToPay = ((valueMax / 100) * 90) - valueNow;
+        }
+        else
+        {
+            valueToPay = valueMax - valueNow;
+        }
+
+        return new Money(valueToPay);
+    }
+
+    public void repair(Money price)
+    {
+        Money repairValue = repairPrice();
+        if(repairValue.getValueFloat() <= price.getValueFloat() && (repairValue.getValueFloat()+1) >= price.getValueFloat())
+        {
+            status = 100f;
+        }
+    }
 }
