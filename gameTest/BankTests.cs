f77efdeffef1c5815e8ad60a4f238eaef7b74f52
Beat94 <36741096+Beat94@users.noreply.github.com>
2023-11-06 23:26:26 +0100

    fixed getMaxCreditValue-Function

--- gameTest/BankTests.cs
+++ gameTest/BankTests.cs
@@ -4,9 +4,13 @@ public class BankTests
 {
     Bank bank = new Bank("Public Bank", 105,102,116,110);
 
-    [Fact]
-    public void test()
+    [Theory]
+    [InlineData(30000, 1000)]
+    [InlineData(0, 0)]
+    [InlineData(0, -1000)]
+    public void getMaxCreditValueTest(float maxCreditValue, float savingValue)
     {
-        
+        float creditValue = bank.getMaxCreditValue(savingValue);
+        Assert.Equal(maxCreditValue, creditValue);
     }
 }
