cf7eac142f0d7fe7e99d1c4bec0b492996a9e15f
Beat94 <36741096+Beat94@users.noreply.github.com>
2024-01-01 15:55:16 +0100

    correction assetmanager

--- cli-game/Savegame.cs
+++ cli-game/Savegame.cs
@@ -2,6 +2,5 @@ public class Savegame
 {
     public StandardJobsManager job {get; set;}
     public Person player {get; set;}
-    public AssetManager assets {get; set;}
     public Bank bankAccount {get; set;}
 }
